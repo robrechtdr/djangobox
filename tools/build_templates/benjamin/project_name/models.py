fe935309c892eff4d33ef31fd1726c6bbbbe1b5c
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-01-27 12:03:55 +0100

    Forgot to set up the admin for benjamin

--- tools/build_templates/benjamin/project_name/models.py
+++ tools/build_templates/benjamin/project_name/models.py
@@ -1,10 +1,10 @@
 from django.db import models
 
 class ModelBox(models.Model):
-    name = models.TextField()
-    #height = 
-    #has_color
+    name = models.CharField(max_length=30)
+    height = models.IntegerField() 
+    has_color = models.BooleanField()
 
     def __unicode__(self):
-        return self.name
+        return u"%s" % self.name
 
