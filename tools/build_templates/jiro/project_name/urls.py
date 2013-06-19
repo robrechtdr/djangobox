2e876948d9b6c68dbbd19e0ef5c11d2921267ca5
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-06-19 15:26:28 +0200

    tc

--- tools/build_templates/jiro/project_name/urls.py
+++ tools/build_templates/jiro/project_name/urls.py
@@ -5,5 +5,5 @@ from apps.core.views import viewbox
 
 urlpatterns = patterns('',
     url(regex=r'^$', view=viewbox, name="viewbox"),
-    url(regex=r'^admin/', view=include(admin.site.urls), name="admin"),
+    url(regex=r'^admin/', view=include(admin.site.urls)),
 )
