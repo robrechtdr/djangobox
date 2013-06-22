338790a6f30d23aec8dc2e3a9a9a94c97c7e205a
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-06-22 13:44:22 +0200

    tc

--- tools/build_templates/jiro/project_name/settings/common.py
+++ tools/build_templates/jiro/project_name/settings/common.py
@@ -98,6 +98,15 @@ INSTALLED_APPS = (
     # 'django.contrib.admindocs',
 )
 
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    # Uncomment the next line for simple clickjacking protection:
+    # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+)
 
 # A sample logging configuration. The only tangible logging
 # performed by this configuration is to send an email to
