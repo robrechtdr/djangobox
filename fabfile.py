90f11496a636b8a9765a07e9ae5f54dff1ce181e
Robrecht De Rouck <Robrecht.De.Rouck@gmail.com>
2013-07-10 20:58:06 +0200

    tc

--- fabfile.py
+++ fabfile.py
@@ -36,36 +36,6 @@ def build_jiro(project_name):
     local('mv %s box/%s' % (project_name, project_name))
 
 
-def build_rimururu(project_name, app_name='core'):
-    build_pinax_zero(project_name)
-    inner_project_path = os.path.join(project_name, project_name)
-    os.chdir(os.path.join(djangobox_dir, inner_project_path))
-    local('django-admin.py startapp %s' % app_name)
-    os.chdir(djangobox_dir)
-    app_path = os.path.join(inner_project_path, app_name)
-    viewbox_path = os.path.join(inner_project_path, 'templates',
-      'viewbox.html')
-    templater.create_template('tools/build_templates/rimururu/'
-      'rimururu_viewbox.html', viewbox_path, {})
-    views_path = os.path.join(app_path, "views.py")
-    templater.create_template('tools/build_templates/rimururu/'
-      'rimururu_views.py', views_path, {'project_name': project_name,
-      'app_name': app_name})
-    urls_path = os.path.join(inner_project_path, "urls.py")
-    templater.create_template('tools/build_templates/rimururu/'
-      'rimururu_urls.py', urls_path, {'project_name': project_name,
-      'app_name': app_name})
-    safe_create_box_dir()
-    local('mv %s box/%s' % (project_name, project_name))
-
-
-def build_pinax_zero(project_name):
-    local('django-admin.py startproject --template=https://github.com/'
-      'pinax/pinax-project-account/zipball/master %s' % project_name)
-    safe_create_box_dir()
-    local('mv %s box/%s' % (project_name, project_name))
-
-
 def build_single_file(inp, out, project_name, app_name=''):
     templater.create_template(inp, out, {'project_name': project_name,
       'app_name': app_name})
--- tools/build_templates/rimururu/rimururu_views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render_to_response
-from django.template import RequestContext
-
-def viewbox(request):
-    name = "Rimururu"
-    return render_to_response('viewbox.html', {'name': name},
-      context_instance=RequestContext(request))
-
